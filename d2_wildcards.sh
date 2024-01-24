f3f8c9447a4eae639118ab52bcd64b7362595dbb
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-24 09:38:05 +0100

    Create d2_wildcards.sh

---
+++ d2_wildcards.sh
@@ -0,0 +1,8 @@
+# 1) list all files in folder ending with _fall
+  ls -l *_fall
+# 2) list all files starting with r or t
+  ls -l [rt]*
+# 3) list all files starting with anything but r or t
+  ls -l [!rt]*
+# 4) all files starting with dirt followed by one character and then have the string "ca" followed by any number of letters
+  ls -l dirt?ca*
