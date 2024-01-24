d40404a1672b3a5348b2285162b1ad8e0e90a3e7
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-24 13:20:20 +0100

    Create d2_amiRoot.sh

---
+++ d2_amiRoot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Zisti, ci spustam script ako Root, alebo ako iny user
+
+if [ "$(id -u)" -eq 0 ]
+
+then
+echo "You are running this script as a Root."
+
+else
+echo "You are not running this script as a Root. Please, use sudo or su."
+
+fi
