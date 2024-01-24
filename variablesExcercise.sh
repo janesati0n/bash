b3b1a1adcccc25bde0fd5705e326dbb081bbd0e8
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-24 08:52:07 +0100

    Create variablesExcercise.sh

---
+++ variablesExcercise.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Urobit 5 variables a vypisat potom
+
+name=$1
+wiw=$2
+password=$3
+lang=$4
+mail=$5
+
+echo "Name is: $name"
+echo "Wiw is: $wiw"
+echo "Password is $password"
+echo "Language set is: $lang"
+echo "E-mail: $mail"
