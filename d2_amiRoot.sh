007e4352add9ddf6c9d4b0646ae21c0baf79d182
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-24 13:27:34 +0100

    Update d2_amiRoot.sh

--- d2_amiRoot.sh
+++ d2_amiRoot.sh
@@ -2,11 +2,11 @@
 # Zisti, ci spustam script ako Root, alebo ako iny user
 
 if [ "$(id -u)" -eq 0 ]
+        
+        then
+        echo "You are running this script as a Root."
 
-then
-echo "You are running this script as a Root."
-
-else
-echo "You are not running this script as a Root. Please, use sudo or su."
+        else
+        echo "You are not running this script as a Root. Please, use sudo or su."
 
 fi
