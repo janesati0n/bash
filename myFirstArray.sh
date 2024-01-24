409f33619059f78ddb17dce571d7e8f44fc14042
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-24 08:56:57 +0100

    Create myFirstArray.sh

---
+++ myFirstArray.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+# Pole s piatimi hodnotami a script vypise druhy element v poradi
+
+formulky=('ferrari' 'red bull' 'mercedes' 'mclaren' 'aston martin')
+
+echo "Druha formulka v poradi je ${formulky[1]}"
