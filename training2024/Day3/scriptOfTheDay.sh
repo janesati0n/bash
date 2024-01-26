c03c19b3062364c8f08af2485702037f18798a31
janesati0n <109526766+janesati0n@users.noreply.github.com>
2024-01-26 09:18:41 +0100

    Update scriptOfTheDay.sh

--- training2024/Day3/scriptOfTheDay.sh
+++ training2024/Day3/scriptOfTheDay.sh
@@ -25,11 +25,7 @@ mkdir -p "$backup_dir"
 rsync -a --exclude="$backup_dir" "$input_dir/" "$backup_dir/"
 
 # Archive the entire backup directory
-archive_file="${input_dir}_backup_full.tar.gz"
+archive_file="${input_dir}_backup.tar.gz"
 tar -czf "$archive_file" -C "$(dirname "$input_dir")" "$(basename "$backup_dir")"
-
-# Archive the backup directory
-archive_file="$input_dir/backup.tar.gz"
-tar -czf "$archive_file" -C "$input_dir" backup
 
 echo "Backup completed. Archive created: $archive_file"
